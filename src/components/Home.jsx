6ec5cd31f1e0cd20788ec0e7b438e94447fc7426
KavinishaAnand <kavinisha.ananthan88202@gmail.com>
2025-04-07 12:06:19 +0530

    test-git

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <div>
+            <p>kavinisha Ananthan</p>
             <p>Home components</p>
             <p>Let's go buddy</p>
 
